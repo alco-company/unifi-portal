4a6d033709380fb5f8e71b20973c47aeb3b462f4
Walther H Diechmann <walther@diechmann.net>
2025-06-10 10:30:54 +0200

    use Cuprite for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,19 @@
+# require "test_helper"
+
+# class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+#   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+# end
 require "test_helper"
+require "test_helpers/capybara_setup"
+require "test_helpers/cuprite_helpers"
+require "test_helpers/cuprite_setup"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :cuprite, using: :chromium, screen_size: [ 1400, 1400 ], options: {
+    js_errors: true,
+    slowmo: ENV["SLOWMO"]&.to_f
+   }
+
+  include Warden::Test::Helpers
+  include CupriteHelpers
 end
